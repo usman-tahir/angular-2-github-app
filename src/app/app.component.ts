83236ec5d977079507ef7172fe601e764fbaf0a2
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-09 21:11:20 -0400

    updated the rootPage definition in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
-  rootPage = HelloIonicPage;
+  rootPage: any = UsersPage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
